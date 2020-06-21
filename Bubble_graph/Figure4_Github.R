9363bb76e3db8e2e5cf240203d3d42be5ae8e589
Nguyen Khoi Nguyen <55298396+BioKhoi@users.noreply.github.com>
2020-06-20 23:25:25 -0600

    Add files via upload

---
+++ Bubble_graph/Figure4_Github.R
@@ -0,0 +1,23 @@
+library(ggplot2)
+
+data(mpg, package="ggplot2")
+
+mpg <- read.csv("/Users/Nguyenphuong/Documents/Shanenguyen/MSc_study_R/FYBER/Arabino/Bubble/template_Sig_taxa.csv")
+
+Percent = mpg$Absolute.Change
+Response = as.factor(mpg$Type)
+
+p = ggplot(mpg, aes(Sample,Taxa, color=Response, shape=Response, fill=Response)) +
+  geom_point(aes(size=Percent)) +
+  scale_color_manual(values = c("black", "red","black")) +
+  scale_shape_manual(values = c(21,21,4)) +
+  scale_fill_manual (values = c("black", "orangered","black")) +
+  scale_size(range = c(0.01, 12)) +
+  theme_bw() +
+  theme(panel.grid.major=element_line(linetype=3,color="white"), 
+       
+        axis.text.y = element_text(size = 14),
+        axis.text.x = element_text(size = 12), 
+        legend.text = element_text(size = 18),
+        legend.position = "bottom")
+plot(p)
